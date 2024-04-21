ae5d6ac67d4c9f3ec9a5122192d9c2846b086b3d
yasir-dev1 <112517455+yasir-dev1@users.noreply.github.com>
2024-04-21 17:31:31 +0300

    Add code to send masseges to gemini and get result

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import dotenv,os
+import dotenv,os,ai
 import telebot as tb
 
 dotenv.load_dotenv()
@@ -10,6 +10,10 @@ bot = tb.TeleBot(token)
 @bot.message_handler(commands=['start', 'hello'])
 def send_welcome(message):
     bot.reply_to(message, "Hi, how can I help you?")
+
+@bot.message_handler(func=lambda msg: True)
+def echo_all(message):
+    bot.reply_to(message, ai.send(message.text))
 
 
 
